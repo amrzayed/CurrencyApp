776c6fe32fb5c763a77d01939a72b66cd1ffa1ac
aabdelhalim <aabdelhalim@ipmagix.com>
2023-06-02 18:29:55 +0300

    Initial Commit

---
+++ CurrencyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyApp
+//
+//  Created by amr new mac on 02/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
